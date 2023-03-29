e8488c22dc0c07ce4cda4570c03b8b0fb6cfbfc2
iamapusaha <apusaha392@gmail.com>
2023-03-30 02:16:02 +0600

    saved cart to localStorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
-    // console.log(cart)
+
+
     let totalPrice = 0;
     let totalShipping = 0;
     for (const product of cart) {
         totalPrice = totalPrice + product.price;
         totalShipping = totalShipping + product.shipping;
     }
+
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
     return (
@@ -17,8 +19,8 @@ const Cart = ({ cart }) => {
             <p>Selected Items: {cart.length}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
-            <p>Tax: ${tax}</p>
-            <h3>Grand Total: ${grandTotal}</h3>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
         </div>
     );
 };
