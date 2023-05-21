65fb75a255b09470e511af3cf2f3338094569531
iamapusaha <apusaha392@gmail.com>
2023-05-21 10:04:13 +0600

    route done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
+                <Link to="/">Home</Link>
                 <Link to="shop">Shop</Link>
                 <Link to="orders">Order</Link>
                 <Link to="inventory">Inventory</Link>
